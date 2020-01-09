2944560422ff989ed6edad429d13fcdd46b3916e
denisprado <denisforigo@gmail.com>
2020-01-09 15:50:12 -0300

    Remove Instafeed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Feverestival',
+    siteUrl: 'https://feverestival-dev.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
